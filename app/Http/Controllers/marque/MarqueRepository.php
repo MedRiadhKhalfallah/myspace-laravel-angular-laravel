7b79de21433e456c44ae44bca6802563962275ae
MedRiadhKhalfallah <mrk19933@gmail.com>
2020-12-29 23:26:17 +0100

    first pagination

--- app/Http/Controllers/marque/MarqueRepository.php
+++ app/Http/Controllers/marque/MarqueRepository.php
@@ -8,8 +8,17 @@ use App\Models\Marque;
 
 class MarqueRepository
 {
+    private $offset = 0;
+    private $limit = 50;
+
     public function searchWithCriteria($criteria)
     {
+        if (isset($criteria['offset'])) {
+            $this->offset = $criteria['offset'];
+        }
+        if (isset($criteria['limit']) && $criteria['limit'] < 50) {
+            $this->limit = $criteria['limit'];
+        }
         $qr = Marque::orderBy('name');
 //        return $criteria;
         foreach ($criteria as $key => $value) {
@@ -22,7 +31,7 @@ class MarqueRepository
 
             }
         }
-        return $qr->get()
+        return $qr->offset($this->offset)->limit($this->limit)->get()
             ->map->format();
         /*        $marques = $qr->get()
                     ->map(function ($marque) {
